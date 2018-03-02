54f0657c6349c64b79a98ddab533835921de4882
DharaniN <31503566+DharaniN@users.noreply.github.com>
2018-03-02 08:47:34 +0000

    Create gcd.c

---
+++ gcd.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter two number: ");
+    scanf("%d%d",&a,&b);
+
+    while(a!=b)
+    {
+        if(a>b)
+        {
+            a-=b;
+        }
+        else
+        {
+            b-=a;
+        }
+    }
+    printf("GCD value is= %d",a);
+    return 0;
+}
+
